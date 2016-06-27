82cac5cc4e727ef921306b32c974bfc5f061860c
Andrew Carl <chuckles.theman@gmail.com>
2016-06-27 17:30:11 -0400

    renamed decorator and generic to more accurately reflect what they do.

---
+++ AtumsoftManager.py
@@ -0,0 +1,11 @@
+"""
+Generic implementation of tunTap base. Import this for a platform independent experience
+"""
+import AtumsoftGeneric
+
+
+# TODO: add required properties here
+
+
+class AtumsoftManager(AtumsoftGeneric.AtumsoftGeneric):
+    pass
